b43a4d2cf6b196494fe42ab7d8f1e84f59096eb8
khartist <tnkhanh2909@gmail.com>
2024-02-26 23:56:07 +0700

    add sound effect and hit effect

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+'''
+Beach by Sakura Girl | https://soundcloud.com/sakuragirl_official
+Music promoted by https://www.chosic.com/free-music/all/
+Creative Commons CC BY 3.0
+https://creativecommons.org/licenses/by/3.0/
+'''
+
 import pygame
 from pygame.locals import *
 import pygame.freetype
 import random
 import time
+import sys
 import math
 
 # init game
 pygame.init()
+pygame.mixer.init()
 # screen_info = pygame.display.Info()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
@@ -24,7 +33,10 @@ FONT_SIZE = 30
 
 hit = 0
 miss = 0
-
+bgm = pygame.mixer.Sound("music/sakura_girl.mp3")
+hammer_click = pygame.mixer.Sound("music/punch.wav")
+hammer_hit = pygame.mixer.Sound("music/punch2.wav")
+bgm.set_volume(0.35)
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 pygame.display.set_caption("Zombie Smash")
 clock = pygame.time.Clock()
@@ -32,6 +44,13 @@ clock = pygame.time.Clock()
 mouse_pos = (0, 0)
 pygame.mouse.set_visible(False)
 font = pygame.font.Font(None, FONT_SIZE)
+bgm_channel = pygame.mixer.Channel(0)
+hammer_click_channel = pygame.mixer.Channel(1)
+hammer_hit_channel = pygame.mixer.Channel(2)
+
+bgm_channel.play(bgm ,loops = -1)
+pygame.mixer.music.set_volume(0.5)  # Adjust volume to 50%
+
 
 
 background_image = pygame.image.load("img/bg.png").convert_alpha()
@@ -45,15 +64,23 @@ for i in range(1, 3):
 hammer_img = hammer[0]
 hammer_rect = hammer_img.get_rect()    
 
+fi_img = pygame.image.load('img/zombie.png').convert_alpha()
+fi_img = pygame.transform.scale_by(fi_img, 0.25)
 
+se_img = pygame.image.load('img/zombie_after.png').convert_alpha()
+se_img = pygame.transform.scale_by(se_img, 0.25)
 
 class Zombie(pygame.sprite.Sprite):
     init_x = 0
     init_y = 0
+
+
+
+
     def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.image.load('img/zombie.png').convert_alpha()
-        self.image = pygame.transform.scale_by(self.image, 0.25)
+        self.image = fi_img
+
         self.rect = self.image.get_rect()
 
         self.rect.x = x
@@ -62,22 +89,41 @@ class Zombie(pygame.sprite.Sprite):
         self.init_y = y
 
         self.appear_time = random.randint(1000, 4000)
-        self.disappear_time = random.randint(1000, 4000)
+        self.disappear_time = 1500
         self.last_appear_time = pygame.time.get_ticks()
 
-
+        #self.delay = 150
+     #   self.get_hit = 0
     def get_clicked(self):
-        self.rect.x = -1000
         current_time = pygame.time.get_ticks()
+       # self.get_hit = current_time
+        hammer_hit_channel.play(hammer_hit, loops = 0)
+        self.image = se_img
+      #  alpha = int(max(0, min(255, 255 - (255 / self.delay) * 150)))
+       # self.image.set_alpha(alpha)
+        #if current_time - self.get_hit >= self.delay:
+            #self.rect.x = -1000
         self.last_appear_time = current_time
-        self.disappear_time = random.randint(1000, 1500)
+        self.appear_time = random.randint(1000, 4000)
+     #   self.disappear_time = random.randint(1000, 1500)
+           # self.image = fi_img
+
+
+
+
     def update(self):
         current_time = pygame.time.get_ticks()
+        '''alpha = int((math.sin(self.appear_time / 1000.0) + 1) * 255) % 255
+        print(alpha)
+        self.image.set_alpha(alpha)'''
         if current_time - self.last_appear_time > self.appear_time:
             # Make sprite disappear
             self.rect.x = -1000  # Move sprite off-screen
+           # self.image = fi_img
             if current_time - self.last_appear_time > self.appear_time + self.disappear_time:
                 # Reset sprite position after disappear_time
+                self.image.set_alpha(255)
+                self.image = fi_img
                 self.rect.x = self.init_x
                 self.rect.y = self.init_y
                 self.last_appear_time = current_time
@@ -115,6 +161,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == MOUSEBUTTONDOWN:
+            hammer_click_channel.play(hammer_click, loops=0)
             if event.button == 1:
                 hammer_img = hammer[1]
                 clicked_sprites = [sprite for sprite in all_sprites if sprite.rect.collidepoint(event.pos)]
@@ -122,6 +169,7 @@ while running:
                     miss +=1
                 else:
                     for sprite in clicked_sprites:
+                #        sprite.get_hit = pygame.time.get_ticks()
                         sprite.get_clicked()
                         hit += 1
         if event.type == MOUSEBUTTONUP:
@@ -147,4 +195,5 @@ while running:
     pygame.display.flip()
     clock.tick(60)
 print(hit)
+pygame.mixer.music.stop()
 pygame.quit()
